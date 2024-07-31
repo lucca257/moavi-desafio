272c55f60c393ab22d6c20fa4bd6ace11bdc3a56
One Code Man <pedro.lucca@live.estacio.br>
2024-07-31 16:37:59 -0300

    chore: adding vuejs

---
+++ resources/js/components/app.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>hellou teste</h1>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Infrastructure\Http\Controllers\ImportController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('welcome');
 });
